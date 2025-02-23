54ae5ecfb0a5f5c81596f368815436ff56175b25
LittleSqu1rrel <bilichenckosasha@gmail.com>
2025-02-23 19:54:12 +0400

    + money)

--- JavaStart.java
+++ JavaStart.java
@@ -2,8 +2,10 @@ public class JavaStart {
     public static void main(String[] args) {
         String name = "Sasha";
         int age = 18;
+        float money = 12.75f;
+
 
         System.out.println("Hello World");
-        System.out.println("My name is " + name + " and I am " + age + " years old");
+        System.out.println("My name is " + name + " and I am " + age + " years old." + "I have " + money + " dollars.");
     }
 }
