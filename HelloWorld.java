d025fe2354f548122538d8c4fbda3518e49b7168
LittleSqu1rrel <bilichenckosasha@gmail.com>
2025-02-23 19:40:26 +0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My name is Sasha");
-
+//        My
+//        name is sahsa
         System.out.print("Hello World");
         System.out.print("My name is Sasha");
     }
