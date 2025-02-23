5ca6543ff4350316671b6ec6ec134054a1fb8037
LittleSqu1rrel <bilichenckosasha@gmail.com>
2025-02-23 19:47:34 +0400

    Variables

---
+++ JavaStart.java
@@ -0,0 +1,9 @@
+public class JavaStart {
+    public static void main(String[] args) {
+        String name = "Sasha";
+        int age = 18;
+
+        System.out.println("Hello World");
+        System.out.println("My name is " + name + " and I am " + age + " years old");
+    }
+}
